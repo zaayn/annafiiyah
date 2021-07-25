16004518f42e14ea922ab7dfd2f50be681675267
Zayn <33050202+zaayn@users.noreply.github.com>
2021-07-25 11:34:05 +0700

    resolve route

--- resources/views/includes/main_page/navbar.blade.php
+++ resources/views/includes/main_page/navbar.blade.php
@@ -19,7 +19,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-2">
-                    <div id="fh5co-logo"><a href="index.html"><i class="icon-study"></i>Annafiiyah<span>.</span></a></div>
+                    <div id="fh5co-logo"><a href="/"><i class="icon-study"></i>Annafiiyah<span>.</span></a></div>
                 </div>
                 <div class="col-xs-10 text-right menu-1">
                     <ul>
